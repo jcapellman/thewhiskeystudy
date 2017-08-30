df7293ca31c7a9c82cfa4ffba96baa4da6d217f6
Jarred Capellman <jcapellman@hotmail.com>
2017-08-29 21:17:26 -0500

    Added: Leaderboard Controller and View

---
+++ src/thewhiskeystudy/Controllers/LeaderboardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace thewhiskeystudy.Controllers
+{
+    public class LeaderboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/thewhiskeystudy/Views/Leaderboard/Index.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace thewhiskeystudy.Views.Leaderboard
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

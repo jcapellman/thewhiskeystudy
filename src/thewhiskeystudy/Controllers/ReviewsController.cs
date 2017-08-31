cffce6b33d6a3c566d105fdbd898b41a272d936b
Jarred Capellman <jcapellman@hotmail.com>
2017-08-30 20:30:05 -0500

    Added: Reviews

---
+++ src/thewhiskeystudy/Controllers/ReviewsController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace thewhiskeystudy.Controllers
+{
+    public class ReviewsController : Controller
+    {
+        public IActionResult Index() => View();
+    }
+}

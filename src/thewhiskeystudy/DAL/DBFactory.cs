4df52548c313b8165f1f4a2fa72c723e0ee1e274
Jarred Capellman <jcapellman@hotmail.com>
2017-09-03 13:57:49 -0500

    Added: Active Field Filtering in the Base DAL Method

--- src/thewhiskeystudy/DAL/DBFactory.cs
+++ src/thewhiskeystudy/DAL/DBFactory.cs
@@ -11,8 +11,8 @@ namespace thewhiskeystudy.DAL
 {
     public class DBFactory : DbContext
     {
-        public DbSet<Reviews> Reviews { get; set; }
+        public DbSet<Reviews> Reviews { get; set; }        
 
-        public List<T> SelectMany<T>(Expression<Func<T, bool>> expression = null) where T : BaseTable => Set<T>().Where(expression).ToList();
+        public List<T> SelectMany<T>(Expression<Func<T, bool>> expression = null) where T : BaseTable => Set<T>().Where(a => a.Active).Where(expression).ToList();
     }
 }

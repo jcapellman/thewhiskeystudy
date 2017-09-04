eebb81d1f8c04272b6061dd0c2d3c56baeabb9c3
Jarred Capellman <jcapellman@hotmail.com>
2017-09-03 20:14:00 -0500

    Fix: Null reference in the DBFactory and SQLite DB Access

--- src/thewhiskeystudy/DAL/DBFactory.cs
+++ src/thewhiskeystudy/DAL/DBFactory.cs
@@ -13,6 +13,12 @@ namespace thewhiskeystudy.DAL
     {
         public DbSet<Reviews> Reviews { get; set; }        
 
-        public List<T> SelectMany<T>(Expression<Func<T, bool>> expression = null) where T : BaseTable => Set<T>().Where(a => a.Active).Where(expression).ToList();
+        public List<T> SelectMany<T>(Expression<Func<T, bool>> expression = null) where T : BaseTable => 
+            (expression == null ? Set<T>().Where(a => a.Active).ToList() : Set<T>().Where(a => a.Active).Where(expression).ToList());
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=tws.db");
+        }
     }
 }

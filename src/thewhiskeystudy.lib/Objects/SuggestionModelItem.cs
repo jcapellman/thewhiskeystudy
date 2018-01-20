5a810ffb263437ce88a05fd995566a0a91fb46c0
Jarred Capellman <jcapellman@hotmail.com>
2018-01-20 14:26:19 -0600

    Added: SuggestionModelItem Class

---
+++ src/thewhiskeystudy.lib/Objects/SuggestionModelItem.cs
@@ -0,0 +1,23 @@
+﻿namespace thewhiskeystudy.lib.Objects
+{
+    public class SuggestionModelItem
+    {
+        public string Name { get; set; }
+
+        public string Aged { get; set; }
+
+        public double ABV { get; set; }
+
+        public bool EasyToFind { get; set; }
+
+        public string TastingNotes { get; set; }
+
+        public string Nose { get; set; }
+
+        public int Price { get; set; }
+
+        public bool WorthIt { get; set; }
+
+        public string AdditionalNotes { get; set; }
+    }
+}

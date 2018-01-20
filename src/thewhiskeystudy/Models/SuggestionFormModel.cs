bfb28bb443a5b5c7a41e6366b8abc50f7cfe29d7
Jarred Capellman <jcapellman@hotmail.com>
2018-01-20 14:26:39 -0600

    Added: SuggestionFormModel Class

---
+++ src/thewhiskeystudy/Models/SuggestionFormModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace thewhiskeystudy.Models
+{
+    public class SuggestionFormModel
+    {
+        [Display(Name = "Max Price in USD? (if any)")]
+        public double? MaxPrice { get; set; }
+
+        [Display(Name = "Like Rye/Spice Flavor?")]
+        public bool LikesRye { get; set; }
+
+        [Display(Name = "Like Smoothness?")]
+        public bool LikesSmooth { get; set; }
+
+        [Display(Name="Like Caramel flavor?")]
+        public bool LikesCaramel { get; set; }
+
+        [Display(Name="Want it to be easy to find?")]
+        public bool WantsReadilyAvailable { get; set; }
+
+        [Display(Name = "Like Sweet flavor?")]
+        public bool LikesSweet { get; set; }  
+        
+        [Display(Name = "Want it higher proof?")]
+        public bool LikesHighProof { get; set; }
+    }
+}

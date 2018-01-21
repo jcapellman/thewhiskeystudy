9e1011b92c644777bb5c98c70b99032a773c3e49
Jarred Capellman <jcapellman@hotmail.com>
2018-01-21 08:25:26 -0600

    Added: Initial work on the XLSX -> JSON DB converter

---
+++ src/thewhiskeystudy.utils.jsonbuilder/FileParser.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+
+using ExcelDataReader;
+
+using Newtonsoft.Json;
+
+using thewhiskeystudy.lib.Objects;
+
+namespace thewhiskeystudy.utils.jsonbuilder
+{
+    public class FileParser
+    {
+        public void ParseFile(string fileName, string outputFileName)
+        {
+            var data = GetDataFromExcel(fileName);
+
+            WriteToJson(data, outputFileName);
+        }
+
+        private void WriteToJson(List<RawDatabaseItem> data, string outputFileName)
+        {
+            var jsonString = JsonConvert.SerializeObject(data);
+
+            File.WriteAllText(outputFileName, jsonString);
+        }
+
+        private List<RawDatabaseItem> GetDataFromExcel(string fileName)
+        {
+            using (var stream = File.Open(fileName, FileMode.Open, FileAccess.Read))
+            {
+                using (var reader = ExcelReaderFactory.CreateReader(stream))
+                {                 
+                    var rows = reader.AsDataSet().Tables[0].Rows.Cast<DataRow>().Skip(1);
+
+                    return rows.Select(row => new RawDatabaseItem(row)).ToList();
+                }
+            }
+        }
+    }
+}
---
+++ src/thewhiskeystudy.utils.jsonbuilder/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+
+using thewhiskeystudy.lib.Common;
+
+namespace thewhiskeystudy.utils.jsonbuilder
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+            var inputFileName = Constants.FILE_XLSX_DBFILENAME;
+            var outputFileName = Constants.FILE_JSON_DBFILENAME;
+
+            if (args.Length == 2)
+            {
+                inputFileName = args[0];
+                outputFileName = args[1];
+            }            
+
+            new FileParser().ParseFile(inputFileName, outputFileName);
+        }
+    }
+}

82e2f06148255fc7bd2c36c18a85d54092f58b89
Jarred Capellman <jcapellman@hotmail.com>
2018-01-21 12:58:45 -0600

    Added: SuggestionFormChoices Enumeration

---
+++ src/thewhiskeystudy.lib/Enums/SuggestionFormChoices.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace thewhiskeystudy.lib.Enums
+{
+    public enum SuggestionFormChoices
+    {
+        [Display(Name = "No Opinion")]
+        NO_OPINION,
+        [Display(Name = "Yes")]
+        YES,
+        [Display(Name = "No")]
+        NO
+    }
+}

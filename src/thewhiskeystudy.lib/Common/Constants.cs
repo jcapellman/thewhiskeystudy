b7c5ea8103efdf7e771ef933a9d20d9477452fbe
Jarred Capellman <jcapellman@hotmail.com>
2018-01-21 08:24:52 -0600

    Added: Constants class

---
+++ src/thewhiskeystudy.lib/Common/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace thewhiskeystudy.lib.Common
+{
+    public static class Constants
+    {
+        public const string FILE_JSON_DBFILENAME = "whiskeydb.json";
+        public const string FILE_XLSX_DBFILENAME = "Whiskey DB.xlsx";
+    }
+}

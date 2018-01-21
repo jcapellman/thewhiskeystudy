4c91376b8e86623ce705cbe4783963df0ce4988d
Jarred Capellman <jcapellman@hotmail.com>
2018-01-21 12:58:09 -0600

    Added: Proper filtering based on the enumeration values instead of booleans

--- src/thewhiskeystudy/Managers/DBManager.cs
+++ src/thewhiskeystudy/Managers/DBManager.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using Newtonsoft.Json;
 
 using thewhiskeystudy.lib.Common;
+using thewhiskeystudy.lib.Enums;
 using thewhiskeystudy.lib.Objects;
 
 namespace thewhiskeystudy.Managers
@@ -14,7 +15,7 @@ namespace thewhiskeystudy.Managers
         public List<RawDatabaseItem> GetDatabase() => 
             !File.Exists(Constants.FILE_JSON_DBFILENAME) ? new List<RawDatabaseItem>() : JsonConvert.DeserializeObject<List<RawDatabaseItem>>(File.ReadAllText(Constants.FILE_JSON_DBFILENAME));        
 
-        public IQueryable<RawDatabaseItem> GetSuggestions(bool wantsReadilyAvailable, bool likesCaramel, bool likesSpice, double? maxPrice, bool likesHighProof, bool likesSmooth, bool likesSweet)
+        public IQueryable<RawDatabaseItem> GetSuggestions(SuggestionFormChoices wantsReadilyAvailable, SuggestionFormChoices likesCaramel, SuggestionFormChoices likesSpice, double? maxPrice, SuggestionFormChoices likesHighProof, SuggestionFormChoices likesSmooth, SuggestionFormChoices likesSweet)
         {
             var query = GetDatabase().AsQueryable();
 
@@ -23,9 +24,52 @@ namespace thewhiskeystudy.Managers
                 query = query.Where(a => a.Price <= maxPrice.Value);
             }
 
-            query = likesHighProof ? query.Where(a => a.ABV > 50.0f) : query.Where(a => a.ABV <= 50.0f);
+            switch (likesHighProof)
+            {
+                case SuggestionFormChoices.YES:
+                    query = query.Where(a => a.ABV >= 50);
+                    break;
+                case SuggestionFormChoices.NO:
+                    query = query.Where(a => a.ABV < 50);
+                    break;                                    
+            }
 
-            return query.Where(a => a.EasyToFind == wantsReadilyAvailable && a.HasCaramelNose == likesCaramel && a.HasCaramelTaste == likesCaramel && a.HasSpiceTaste == likesSpice && a.HasSmoothTaste == likesSmooth && a.HasSweetTaste == likesSweet);
+            if (wantsReadilyAvailable != SuggestionFormChoices.NO_OPINION)
+            {
+                query = wantsReadilyAvailable == SuggestionFormChoices.YES
+                    ? query.Where(a => a.EasyToFind)
+                    : query.Where(a => !a.EasyToFind);
+            }
+
+            if (likesCaramel != SuggestionFormChoices.NO_OPINION)
+            {
+                query = likesCaramel == SuggestionFormChoices.YES
+                    ? query.Where(a => a.HasCaramelNose || a.HasCaramelTaste)
+                    : query.Where(a => !a.HasCaramelNose && !a.HasCaramelTaste);
+            }
+
+            if (likesSpice != SuggestionFormChoices.NO_OPINION)
+            {
+                query = likesSpice == SuggestionFormChoices.YES
+                    ? query.Where(a => a.HasSpiceTaste)
+                    : query.Where(a => !a.HasSpiceTaste);
+            }
+
+            if (likesSmooth != SuggestionFormChoices.NO_OPINION)
+            {
+                query = likesSmooth == SuggestionFormChoices.YES
+                    ? query.Where(a => a.HasSmoothTaste)
+                    : query.Where(a => !a.HasSmoothTaste);
+            }
+
+            if (likesSweet != SuggestionFormChoices.NO_OPINION)
+            {
+                query = likesSweet == SuggestionFormChoices.YES
+                    ? query.Where(a => a.HasSweetTaste)
+                    : query.Where(a => !a.HasSweetTaste);
+            }
+
+            return query;
         }
     }
 }

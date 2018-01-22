8278109cf8044e1cd5befa5d590b0ca432c9978c
Jarred Capellman <jcapellman@hotmail.com>
2018-01-22 06:57:57 -0600

    Added: Initial work on the ApiController class (need to add proper middleware)

---
+++ src/thewhiskeystudy/Controllers/ApiController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using thewhiskeystudy.lib.Common;
+
+namespace thewhiskeystudy.Controllers
+{
+    public class ApiController : Controller
+    {
+        // TODO: Replace with proper auth middleware
+        private bool IsAuthorized(string token) => System.IO.File.ReadAllText(Constants.FILE_TOKEN_FILENAME) == token;
+        
+        [HttpPut]
+        public bool PUT(string jsonDb)
+        {
+            if (!HttpContext.Request.Headers.ContainsKey(Constants.HTTP_HEADER_TOKEN) || !IsAuthorized(HttpContext.Request.Headers[Constants.HTTP_HEADER_TOKEN]))
+            {
+                return false;
+            }
+            
+            System.IO.File.WriteAllText(Constants.FILE_JSON_DBFILENAME, jsonDb);
+
+            return true;
+        }
+    }
+}

23a07de37c55e334002b424c12b70981754caf74
Jarred Capellman <jcapellman@hotmail.com>
2018-01-22 19:09:53 -0600

    Change: Swapped out the temporary middleware for a more proper TokenFilter

---
+++ src/thewhiskeystudy/Middleware/AuthorizationAttribute.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace thewhiskeystudy.Middleware
+{
+    public class TokenFilterAttribute : TypeFilterAttribute
+    {
+        public TokenFilterAttribute() : base(typeof(TokenFilter))
+        {
+        }
+    }
+
+    public class TokenFilter : IAuthorizationFilter
+    {
+        private bool IsAuthorized(string token) => System.IO.File.ReadAllText(lib.Common.Constants.FILE_TOKEN_FILENAME) == token;
+
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            if (!context.HttpContext.Request.Headers.ContainsKey(lib.Common.Constants.HTTP_HEADER_TOKEN) || 
+                !IsAuthorized(context.HttpContext.Request.Headers[lib.Common.Constants.HTTP_HEADER_TOKEN]))
+            {
+                context.Result = new ForbidResult();
+            }
+        }
+    }
+}

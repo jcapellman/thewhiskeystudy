e7d62df6197deec1c02813ef61353835ab137177
Jarred Capellman <jcapellman@hotmail.com>
2018-01-29 21:30:40 -0600

    Added: IMemoryCache to the BaseManager

--- src/thewhiskeystudy/Managers/BaseManager.cs
+++ src/thewhiskeystudy/Managers/BaseManager.cs
@@ -1,6 +1,14 @@
-﻿namespace thewhiskeystudy.Managers
+﻿using Microsoft.Extensions.Caching.Memory;
+
+namespace thewhiskeystudy.Managers
 {
     public class BaseManager
     {
+        protected IMemoryCache cache;
+
+        public BaseManager(IMemoryCache cache)
+        {
+            this.cache = cache;
+        }
     }
 }
